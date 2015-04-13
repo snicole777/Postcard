1c1a9e3c74994b47384f2bf88780650daf31fabc
Sarah Press <sarah@projectfixup.com>
2015-04-13 09:40:31 -0500

    added a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Adding a comment to test a commit
     }
 
 
